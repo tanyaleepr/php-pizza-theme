9dab553819ebd93140da8729d0fd67466d8b8329
tanyaleepr <tanyaleepr@gmail.com>
2022-10-05 11:18:54 -0400

    adding structure towards header

---
+++ wp-content/themes/pizza-template/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+
+
+  <header class="site-header">
+    <div class="logo">
+      <a href="<?php echo esc_url(home_url('/')); ?>">
+        <img src="<?php echo get_template_directory_uri() ?>/img/logo.svg">
+      </a>
+    </div>
+
+    <div class="info-header">
+      <div class="social-media">
+
+      </div>
+      <!--social media -->
+      <div class="direction">
+        <p> 27527 Bay Area Mountains View, CA 37130 </p>
+        <p> Phone #: 555-555-5555 </p>
+
+      </div>
+      <!--direction-->
+    </div>
+    <!--info-header-->
+
+  </header>
--- wp-content/themes/pizza-template/page.php
+++ wp-content/themes/pizza-template/page.php
@@ -1,3 +1,8 @@
+<?php get_header(); ?>
+
+
+
+
 <?php while (have_posts()) : the_post(); ?>
   <h1><?php the_title(); ?></h1>
 
